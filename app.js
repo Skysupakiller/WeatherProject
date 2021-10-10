088373f33b39cad3023a20cf9b1a5784ec758dba
chris <chris.turp1@gmail.com>
2021-10-11 00:02:32 +0200

    Added feature to enter any city and get the weather

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 
 app.get("/", function(req, res) {
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Zurich&appid=7046a7009e64d2964bd78cfcd546b2e5&units=metric";
+  res.sendFile(__dirname + "/index.html");
+
+});
+
+app.post("/", function(req, res) {
+
+  const query = req.body.cityName;
+  const apikey = "7046a7009e64d2964bd78cfcd546b2e5";
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + unit;
   https.get(url, function(response) {
 
     response.on("data", function(data) {
-      const weatherData=JSON.parse(data); //Convert hexa into JSON object
+      const weatherData = JSON.parse(data); //Convert hexa into JSON object
       const temp = weatherData.main.temp;
       const image = weatherData.weather[0].icon;
-      const imageUrl = "http://openweathermap.org/img/wn/"+ image +"@2x.png";
+      const imageUrl = "http://openweathermap.org/img/wn/" + image + "@2x.png";
       const description = weatherData.weather[0].description;
-      res.write("<h1>The temperature in Zurich is " + temp + " degrees Celcius</h1>");
+      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius</h1>");
       res.write("<p>The weather is currently " + description + "</p>");
       res.write("<img src=" + imageUrl + ">");
       res.send();
@@ -26,13 +39,9 @@ app.get("/", function(req, res) {
       // console.log(JSON.stringify(object));
 
     });
-
   });
-  // res.send("server is running");
 });
-
-
-
+// res.send("server is running");
 
 
 
